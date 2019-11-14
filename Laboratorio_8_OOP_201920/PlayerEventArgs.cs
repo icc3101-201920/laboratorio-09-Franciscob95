30f0e4e646459d311b71f761bdf595c8e4800e29
Franciscob95 <38050199+Franciscob95@users.noreply.github.com>
2019-11-14 18:29:18 -0300

    Add files via upload

---
+++ Laboratorio_8_OOP_201920/PlayerEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_8_OOP_201920
+{
+    public class PlayerEventArgs:EventArgs
+    {
+        public Cards.Card Card { get; set; }
+        public Player Player { get; set; }
+    }
+}
